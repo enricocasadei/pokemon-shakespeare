44e399133715c4adcba44eb12c594612c00f398c
enrico <enricocasadei.enrico@gmail.com>
2021-05-22 08:35:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Commands
+
+### Prestart
+
+The first time you run the server you will need to install [mkcert](https://github.com/FiloSottile/mkcert#macos) and run the following command:
+
+    npm run cert:dev
+
+This command adds some keys necessary to run the development on localhost on https
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
+  mode: process.env.NODE_ENV || "development",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "src"),
+    port: 9000,
+    https: true,
+    key: path.join(__dirname + "/localhost-key.pem"),
+    cert: path.join(__dirname + "/localhost.pem")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.(css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};
